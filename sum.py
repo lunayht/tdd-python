108e77ce90fcd5a4d2ea709f017cdd5fcd540e0f
Ying Hui <yhtan927@gmail.com>
2020-10-31 16:10:05 +0800

    add demo function

--- sum.py
+++ sum.py
@@ -1,3 +1,22 @@
+from typing import List
+
+def demo_function(x: int, y: List[str]) -> None:
+    """Print the integer and items in the list
+
+    Args: 
+    x: an integer
+    y: List of string
+    """
+    print(f"The input integer is: {x}")
+    for idx, word in enumerate(y):
+        print(idx, word)
+
 def sum(a: int, b: int) -> int:
+    """Apply addition to two numbers
+    
+    Args:
+    a: an integer
+    b: an integer 
+    """
     # return True
     return a+b
